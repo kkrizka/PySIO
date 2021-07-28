3e85465b107744a510fca38a80fd7bf06d18dd5d
Karol Krizka <kkrizka@gmail.com>
2021-07-28 14:48:39 -0700

    Update version string.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PySIO',
-      version='0.1',
+      version='0.1.0',
       description='Parser for SLAC\'s Serial Input Output file format.',
       author='Karol Krizka',
       author_email='kkrizka@gmail.com',
