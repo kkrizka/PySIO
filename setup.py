5a4fec4f4a24c78fb1a19f8101bc9989ae4198eb
Karol Krizka <kkrizka@gmail.com>
2021-12-21 20:07:54 -0800

    Add missing metadata.

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='PySIO',
-      version='0.1.0',
-      description='Parser for SLAC\'s Serial Input Output file format.',
-      author='Karol Krizka',
-      author_email='kkrizka@gmail.com',
-      packages=['sio'])
+setup()
 
